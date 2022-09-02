73c141ddc1a9d047b7bf0bfdf93b7357968f4018
Oliver Sierra <oliver.sierra@telusinternational.com>
2022-09-02 14:47:26 -0600

    init the project

---
+++ src/main/java/com/telus/dsu/introgit/App.java
@@ -0,0 +1,13 @@
+package com.telus.dsu.introgit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
