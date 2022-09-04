a4272953737b9a035aa3f67516e198458b93bc9c
Oliver Sierra <oliver.sierra@telusinternational.com>
2022-09-03 18:52:28 -0600

    console java

---
+++ src/main/java/com/telus/dsu/introgit/util/Console.java
@@ -0,0 +1,10 @@
+package com.telus.dsu.introgit.util;
+
+public class Console {
+    
+    public static void printTitle(String title){
+        System.out.println("***********************************");
+        System.out.println(title.toUpperCase());
+        System.out.println("***********************************\n");
+    }
+}
